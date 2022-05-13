ae089fcbf25e9e6e116a34b39260bd39ff69abfc
Antonio <antonio.dedios@petroprix.com>
2022-05-13 12:37:18 +0200

    Arreglo url

--- src/components/slider/Slides.js
+++ src/components/slider/Slides.js
@@ -7,7 +7,7 @@ const slidesInfo = [
     src: 'https://cdn.pixabay.com/photo/2020/10/15/14/42/man-5656933_960_720.png',
     alt: 'Project 1',
     desc: 'Heroes APP',
-    href: 'https://paniser.github.io/marvel',
+    href: 'https://paniser.github.io/hero-app',
   },
   {
     src: 'https://cdn.pixabay.com/photo/2016/05/30/14/23/detective-1424831_960_720.png',
